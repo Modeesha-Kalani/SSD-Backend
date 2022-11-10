e22416d437cb154bbd7cc74e68d0ce15ae0fda06
ShalihtaAlahakoon <it19960432@my.sliit.lk>
2022-11-10 10:46:22 +0530

    JWT implementationa

--- server.js
+++ server.js
@@ -3,7 +3,46 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const jwt = require('jsonwebtoken');
+app.use(express.json());
 
+const posts = [
+    {
+        username: 'shalitha',
+        title: 'Post 1'
+    },
+    {
+        username: 'Jane',
+        title: 'Post 2'
+    }
+];
+
+app.get('/posts',authenticateToken,(req, res) => {
+    res.send(posts.filter(post => post.username === req.user.name));
+})
+app.get('/login', (req, res) => {
+    //authenticate user
+
+
+    const username = req.body.username;
+    const user = { name: username };
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json({ accessToken: accessToken });
+    
+})
+
+function authenticateToken(req,res,next){
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if(token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if(err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    })
+}
 
 //Port
 const PORT = process.env.PORT || 8000;
